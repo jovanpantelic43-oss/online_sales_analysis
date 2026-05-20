1a0df9398b70be52948a81c06f86f8d7688859a6
Jovan <jovanpantelic43@gmail.com>
2026-05-20 22:45:02 +0200

    Updated product data in main branch

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
+# Kreiranje ProductManager instance
 manager = ProductManager()
 
-product1 = Product("Laptop", 1200, 5)
-product2 = Product("Mouse", 25, 20)
-product3 = Product("Keyboard", 75, 10)
+# Kreiranje proizvoda
+product1 = Product("Gaming Laptop", 1500, 3)
+product2 = Product("Wireless Mouse", 40, 15)
+product3 = Product("Mechanical Keyboard", 120, 8)
+product4 = Product("Monitor", 300, 5)
 
+# Dodavanje proizvoda u manager
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
+manager.add_product(product4)
 
-print("AVAILABLE PRODUCTS:")
-manager.show_products()
+# Kreiranje korpe
+cart = Cart()
 
-print("\nTOTAL INVENTORY VALUE:")
-print(manager.total_inventory_value(), "$")
+# Dodavanje proizvoda u korpu
+cart.add_to_cart(product1)
+cart.add_to_cart(product2)
+cart.add_to_cart(product3)
+
+# Prikaz sadržaja korpe
+print("\nCART CONTENT:")
+cart.show_cart()
+
+# Ukupna vrednost korpe
+print("\nTOTAL FOR PAYMENT:")
+print(cart.calculate_total(), "$")
